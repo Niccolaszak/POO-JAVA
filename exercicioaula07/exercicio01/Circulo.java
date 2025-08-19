5116144165658a45f299e3c6c604346d0c302774
Nicolas Miguel Uczak <nicolasmiuczak@gmail.com>
2025-08-18 21:09:54 -0300

    Primeira att

---
+++ exercicioaula07/exercicio01/Circulo.java
@@ -0,0 +1,13 @@
+package exercicioaula07.exercicio01;
+
+class Circulo extends Forma {
+    private double raio;
+
+    public double getRaio() {return raio;}   
+    public void setRaio(double raio) {this.raio = raio;}
+
+    public double calcularArea() {
+        return 3.14 * raio * raio;
+    }
+}
+
---
+++ exercicioaula07/exercicio01/Forma.java
@@ -0,0 +1,12 @@
+package exercicioaula07.exercicio01;
+
+class Forma {
+    private String cor;
+    private boolean preenchida ;
+
+    public String getCor() {return cor;}   
+    public void setCor(String cor) {this.cor = cor;}
+    public boolean isPreenchida() {return preenchida;}
+    public void setPreenchida(boolean preenchida) {this.preenchida = preenchida;}
+
+}
